71b4a568c0198dd5a4032c6fd216c3647519005c
sahildkun <sahilsahoo23@gmail.com>
2022-10-15 20:21:08 +0530

    font added

--- src/Hero.js
+++ src/Hero.js
@@ -3,22 +3,21 @@ import spectrum from './images/output-onlinepngtools1.png'
 
 function Hero() {
   return (
-    <div>
+    <div className='bg-[#303636]'>
 
     {/* NAVBAR */}
-    <div className=" relative container mx-auto p-6 bg-black">
+    <div className=" relative container mx-auto p-6 ">
         {/* flex container */}
         <div className='flex items-center justify-between'>
           <div className=''>
-       <img src={spectrum} alt="img"  className=''/>
+       <img src={spectrum} alt="img"  className='h-[] w-[]'/>
     </div>
 
-         <div className=' hidden md:flex flex-auto justify-end space-x-5 items-center'>
-            <div>FEATURES</div>
-            <div>PRICING</div>
-            <div>CONTACT</div>
+         <div className=' hidden md:flex flex-auto justify-end space-x-5 items-center' id='cyber'>
+            
           
-            <button className='border-2 border-[#f34013] bg-white px-3 py-2'>LOGIN</button>
+            <button className='text-white font-cyberian demo'>LOGIN</button>
+            <button className='text-white font-cyberian demo'>REGISTER</button>
          </div>
 
          {/* hamburger menu */}
@@ -26,6 +25,21 @@ function Hero() {
          
          </div>
          {/* MOBILE MENU */}
+
+
+         {/* hero section */}
+
+
+         <section>
+          <div className='container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row'>
+            {/* left item */}
+             <h1>   </h1>
+
+             {/* right image */}
+
+
+          </div>
+         </section>
         
     </div>
     </div>
