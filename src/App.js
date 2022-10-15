19af271ed18a17e52f1fc1116cbec48266528c66
sahildkun <sahilsahoo23@gmail.com>
2022-10-15 16:04:17 +0530

    shuva js updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Hero from './Hero';
-
+import ShuvAnime from './ShuvAnime';
 function App() {
   return (
     <div>
       <Hero/>
+      <ShuvAnime/>
     </div>
   );
 }
---
+++ src/shuvanime.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function shuvanime() {
+  return (
+    <div>
+
+
+<div className="text-2xl">
+    hello world
+</div>
+<img src="" alt="" />
+
+
+       
+    </div>
+  )
+}
+
+export default shuvanime
