a31f150819d22ef3565ef4c9857ab504873bc518
Animesh239 <115821028+Animesh239@users.noreply.github.com>
2022-10-17 23:39:15 +0530

    nere

--- src/ShuvaAnime.js
+++ src/ShuvaAnime.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import rect from './images/Rectangle 10.png'
+import rect1 from './images/Rectangle 19.png'
 
 function ShuvAnime() {
   return (
     <>
       <body>
         <br /><br /><br /><br /><br /><br /><br />
-        <div class="border-b-gray-400  border-b-2 ml-[91px] mr-[80px]">
+         <div class="border-b-gray-400  border-b-2 ml-[91px] mr-[80px]"> 
+         {/* Heading */}
           <p class="text-[#CAC9C9]/[87] w-[1067px] h-[35px]  text-[42px] font-[400] leading-[49px] tracking-[0.025em]  not-italic" id="cyber">
             ALL SOFTWARE, HARDCORE & DESIGN
           </p>
@@ -15,35 +18,42 @@ function ShuvAnime() {
             </div>
             <div class="flex pt-11 gap-[19.2px]">
               <div class="bg-[#E86555] w-[32px] h-[32px] "></div>
-              <div class="bg-[#303636] w-[32px] h-[32px]  border-[1.6px] border-[#CAC9C9]/[87]"></div>
-              <div class="bg-[#303636] w-[32px] h-[32px]  border-[1.6px] border-[#CAC9C9]/[87]"></div>
+              <div class="bg-transparent w-[32px] h-[32px]  border-[1.6px] border-[#CAC9C9]/[87]"></div>
+              <div class="bg-transparent w-[32px] h-[32px]  border-[1.6px] border-[#CAC9C9]/[87]"></div>
             </div>
           </div>
         </div>
-        <div class="flex justify-between  ml-[89.89px] mr-[104.82px] pt-[71.87px] space-x-2 pb-14 ">
 
+         <div class="flex justify-between  ml-[89.89px] mr-[104.82px] pt-[71.87px] space-x-2 pb-14 ">
+          {/* Flex container 1 */}
           <div class="flex flex-col space-y-[33.76px]">
-            <img src="Rectangle 10.png" class="w-[333.08px] h-[245.38px]" alt="image" />
+            <img src={rect} class="w-[333.08px] h-[245.38px]" alt="image" />
+            {/* Image(Rectangle 10.png) */}
             <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">APP DEVELOPMENT</p>
             <p class="text-[#CAC9C9]/[87] w-[333px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic " id="jmh">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore quis nihil alias ullam nesciunt amet quos asperiores nemo ipsam, harum eaque officiis voluptate, repellendus id fugit tenetur. Nostrum quidem at illo, animi expedita sunt! Facilis!</p>
             <button class="flex justify-center bg-[#DA493D] w-[198.43px] h-[41.4px] items-center text-white" id="cyber">REGISTER</button>
           </div>
           <div class="flex flex-col space-y-[33.76px]">
-            <img src="Rectangle 10.png" class="w-[333.08px] h-[245.38px]" alt="image" />
+            <img src={rect} class="w-[333.08px] h-[245.38px]" alt="image" />
+            {/* Image(Rectangle 10.png) */}
             <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">APP DEVELOPMENT</p>
             <p class="text-[#CAC9C9]/[87] w-[333px] h-[108px]  font-[400] text-[14.1734px] leading-[18px]  not-italic" id="jmh">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore quis nihil alias ullam nesciunt amet quos asperiores nemo ipsam, harum eaque officiis voluptate, repellendus id fugit tenetur. Nostrum quidem at illo, animi expedita sunt! Facilis!</p>
             <button class="flex justify-center bg-[#DA493D] w-[198.43px] h-[41.4px] items-center text-white" id="cyber">REGISTER</button>
           </div>
 
           <div class="flex flex-col space-y-[33.76px]">
-            <img src="Rectangle 10.png" class="w-[333.08px] h-[245.38px]" alt="image" />
+            <img src={rect} class="w-[333.08px] h-[245.38px]" alt="image" />
+            {/* Image(Rectangle 10.png) */}
             <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">APP DEVELOPMENT</p>
             <p class="text-[#CAC9C9]/[87] w-[333px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic " id="jmh">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore quis nihil alias ullam nesciunt amet quos asperiores nemo ipsam, harum eaque officiis voluptate, repellendus id fugit tenetur. Nostrum quidem at illo, animi expedita sunt! Facilis!</p>
             <button class="flex justify-center bg-[#DA493D] w-[198.43px] h-[41.4px] items-center text-white" id="cyber">REGISTER</button>
           </div>
-        </div>
-        <br/><br/><br/><br/>
+        </div> 
+
+        <br /><br /><br /><br />
+
         <div class="border-b-gray-400  border-b-2 ml-[91px] mr-[80px]">
+          {/* Heading 2 */}
           <p class="text-[#B6B6B6] w-[934px] h-[39px]  text-[42px] font-[400] leading-[49px] tracking-[0.025em]  not-italic" id="cyber">
             STEPS TO GET STARTED WITH..
           </p>
@@ -51,46 +61,58 @@ function ShuvAnime() {
             <div class="text-[#E5E5E5] w-[600px] h-[92px]  font-[400] text-[102.985px] leading-[119px] tracking-[0.025em] not-italic" id="cyber">
               SID 2022
             </div>
-            
-            </div>
+
           </div>
-        
-        <div/>
-        <div class="text-[#CAC9C9] pt-[22.9px] ml-[91px] mr-[80px] " id="jmh">
-          The internship drive is divided into five types 
         </div>
-        <div class=" pt-[53.88px] ml-[91px] mr-[80px] flex">
-          <div>
-          <img src="Rectangle 19.png" class="w-[310.36px] h-[187px]" alt="image" />
+
+        <div class="text-[#CAC9C9] pt-[22.9px] ml-[91px] mr-[80px] " id="jmh">
+          {/* Text */}
+          The internship drive is divided into five types
+        </div>   
+
+
+
+
+
+        <div class=" pt-[53.88px] ml-[91px] mr-[76px] flex border-b-gray-400  border-b-2 ">
+          {/* Flex container 2 */}
+          <div class="pr-[99.64px]">
+            {/* Image(Rectangle 19.png) */}
+            <img src={rect1} class="w-[310.36px] h-[187px]" alt="image" />
           </div>
-          <div class="flex">
-            <div class="flex-col">
-              <div>
-          <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">REGISTRATION PHASE</p>
-            <p class="text-[#CAC9C9]/[87] w-[333px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic " id="jmh">Students need to register themselves at https://internship.spectrumcet.com . It is mandatory for all the 
-             students to register so as to take part in the internship drive.</p>
-            </div> 
-            <div>
-          <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">TASK 1:-</p>
-            <p class="text-[#CAC9C9]/[87] w-[333px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic " id="jmh">Students need to register themselves at https://internship.spectrumcet.com . It is mandatory for all the 
-             students to register so as to take part in the internship drive.</p>
-            </div></div>
-            <div class="flex-col">
-            <div>
-          <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">TASK 2:-</p>
-            <p class="text-[#CAC9C9]/[87] w-[333px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic " id="jmh">Students need to register themselves at https://internship.spectrumcet.com . It is mandatory for all the 
-             students to register so as to take part in the internship drive.</p>
+
+
+          <div class="flex flex-col">
+            {/* Nested Flex */}
+            <div class="flex mb-[39px]">
+              <div class="border-x-[1.02px] border-[#D9D9D9] px-[25px] pb-[103px] pt-[35px] pr-[60px]">
+                <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic " id="cyber">REGISTRATiON PHASE</p>
+                <p class="text-[#CAC9C9]/[87] w-[288px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic pt-[18.07px] " id="jmh">Students need to register themselves at https://internship.spectrumcet.com . It is mandatory for all the students to register so as to take part in the internship drive.</p>
+              </div>
+              <div class="border-r-[1.02px] border-[#D9D9D9] px-[25px] pb-[103px] pt-[35px] pr-[60px]">
+                <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">TASK 1:-</p>
+                <p class="text-[#CAC9C9]/[87] w-[288px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic pt-[18.07px]" id="jmh">Students need to register themselves at https://internship.spectrumcet.com . It is mandatory for all the students to register so as to take part in the internship drive.</p>
+              </div>
             </div>
-            <div>
-          <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">TASK 3:-</p>
-            <p class="text-[#CAC9C9]/[87] w-[333px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic " id="jmh">Students need to register themselves at https://internship.spectrumcet.com . It is mandatory for all the 
-             students to register so as to take part in the internship drive.</p>
-            </div></div>
+
+              <hr></hr>
+
+            <div class="flex mt-[39px] mb-[39px]">
+              <div class="border-x-[1.02px] border-[#D9D9D9] px-[25px] pb-[103px] pt-[35px] pr-[60px]" >
+                <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">TASK 2:-</p>
+                <p class="text-[#CAC9C9]/[87] w-[288px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic pt-[18.07px] " id="jmh">Students need to register themselves at https://internship.spectrumcet.com . It is mandatory for all the students to register so as to take part in the internship drive.</p>
+              </div>
+              <div class="border-r-[1.02px] border-[#D9D9D9] px-[25px] pb-[103px] pt-[35px] pr-[60px]">
+                <p class="text-[#E5E5E5] w-[386.23px] h-[29.93px]   font-[400] text-[22.4441px] leading-[26px] not-italic" id="cyber">TASK 3:-</p>
+                <p class="text-[#CAC9C9]/[87] w-[288px] h-[108px]   font-[400] text-[14.1734px] leading-[18px]  not-italic  pt-[18.07px]" id="jmh">Students need to register themselves at https://internship.spectrumcet.com . It is mandatory for all the students to register so as to take part in the internship drive.</p>
+              </div>
+            </div>
           </div>
 
         </div>
+        <br></br><br></br><br></br><br></br><br></br>
       </body>
-        {/* mom */}
+      {/* mom */}
       <div />
     </>
   )
