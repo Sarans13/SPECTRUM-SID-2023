4918d7602f536bfc2a11b10ba908d443faa5aca0
Sarans13 <114057996+Sarans13@users.noreply.github.com>
2023-12-07 19:20:56 +0530

    responsiveness

--- src/components/content/Carousel/Products.js
+++ src/components/content/Carousel/Products.js
@@ -1,18 +1,37 @@
-import React from "react";
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink } from 'react-router-dom';
 
 export default function Product(props) {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
+
   return (
     <div className="card">
       <img className="product--image mb-5" src={props.url} alt="product" />
       <h2 id="cyber">{props.name}</h2>
-      <p className="description mt-5" id="jmh">{props.description}</p>
+      <p className="description mt-5" id="jmh">
+        {showFullDescription
+          ? props.description
+          : `${props.description.slice(0, 104)}...`}
+          &nbsp;
+          {!showFullDescription ? (
+          <span className="read-more underline cursor-pointer" onClick={toggleDescription} style={{ color: "#DA493D" }}>
+            Read More
+          </span>
+        ) : (
+          <div className="read-less underline cursor-pointer" onClick={toggleDescription} style={{ color: "#DA493D" }}>
+            Read Less
+          </div>)}
+      </p>
       <p>
-      <button className="mt-5">
-             <NavLink to={'/register'} className='text-white h-12' id='cyber'>
-                REGISTER
-              </NavLink>
-             </button>
+        <button className="mt-5">
+          <NavLink to={'/register'} className='text-white h-12' id='cyber'>
+            REGISTER
+          </NavLink>
+        </button>
       </p>
     </div>
   );
--- src/components/content/Description.js
+++ src/components/content/Description.js
@@ -23,10 +23,10 @@ const Description = () => {
   };
   return (
     <div className='my-20'>
-      <div className='flex flex-col mx-5 md:space-y-10  lg:p-20 md:items-left'>
-        <p id='cyber' className='text-[#B6B6B6] text-lg lg:text-5xl md:text-4xl '>ALL SOFTWARE, HARDWARE & DESIGN</p>
+      <div className='flex flex-col mx-5 md:space-y-10  lg:p-20 md:items-left lg:mb-0 md:mb-20'>
+        <p id='cyber' className='text-[#B6B6B6] text-lg lg:text-5xl md:mx-12 lg:mx-0 md:text-4xl '>ALL SOFTWARE, HARDWARE & DESIGN</p>
         {/* <p id='cyber' className='text-white text-lg md:text-[7rem]  '>DOMAINS</p> */}
-        <div className="forest">
+        <div className="forest md:mx-12 lg:mx-0">
         <p id='cyber' className='text-white text-lg lg:text-[7rem] md:text-[5rem]'>DOMAINS</p>
         <div className="Simpledots">
           <Dots activeIndex={activeIndex} totalDots={product.length}/>
@@ -64,7 +64,7 @@ const Description = () => {
       </div>
 
       {/* <div className='md:mt-20   mx-10 md:mx-20'> */}
-      <div className='md:mt-20 mx-10 md:mx-20'>
+      <div className='md:mt-20 mx-5 md:mx-20'>
         <div className='border-b-2 border-[#CAC9C9]/87  w-auto'></div>
       </div>
 
